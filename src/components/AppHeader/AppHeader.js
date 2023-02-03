88ebd8610309fed38d66169996a84beb0b0c325f
Кирилл Аникеев <k.anikeev@innopolis.ru>
2023-02-03 13:34:24 +0300

    create header

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import styles from './AppHeader.module.css'
+import { BurgerIcon, ListIcon, ProfileIcon, Logo} from '@ya.praktikum/react-developer-burger-ui-components'
 function AppHeader() {
   return (
-    <div>AppHeader</div>
+    <header className={styles.header}>
+        <a className={styles.linkActive}  href="/">
+            <BurgerIcon type="primary" />
+            <p className={styles.text}>Конструктор</p>
+        </a>   
+        <a className={styles.link}  href="/">
+            <ListIcon type="secondary" />
+            <p className={styles.text}>Лента заказов</p>
+        </a>
+        <a className={styles.logo}  href="/">
+            <Logo />
+        </a>
+        <a className={styles.link}  href="/"> 
+            <ProfileIcon type="secondary" />
+            <p className={styles.text}>Личный кабинет</p>
+        </a>
+    </header>
   )
 }
 
