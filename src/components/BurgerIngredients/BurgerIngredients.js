bbdc18c98e1c71a892336d1612cbd6b6aa260036
Кирилл Аникеев <k.anikeev@innopolis.ru>
2023-02-06 13:38:36 +0300

    implement PropTypes

--- src/components/BurgerIngredients/BurgerIngredients.js
+++ src/components/BurgerIngredients/BurgerIngredients.js
@@ -5,11 +5,11 @@ import { data } from '../utils/data'
 import Ingredients from '../Ingredients/Ingredients'
 
 function BurgerIngredients() {
-  const [selectIngredients, setSelectIngredients] = useState('Булочки')
+  const [selectIngredients, setSelectIngredients] = useState('Булки')
   return (
     <div className={styles.main}>
       <h2 className={styles.title}>Соберите бургер</h2>
-      <div className={styles.selector}>
+     <a href={selectIngredients}> <div className={styles.selector}>
       <Tab value="Булки" active={selectIngredients === 'Булки'} onClick={()=>setSelectIngredients("Булки")}>
       Булки
       </Tab>
@@ -19,7 +19,7 @@ function BurgerIngredients() {
       <Tab value="Начинки" active={selectIngredients === 'Начинки'} onClick={()=>setSelectIngredients("Начинки")}>
       Начинки
       </Tab>
-    </div>
+    </div></a>
     <section className={styles. ingredientsTypes}>
     <Ingredients title="Булки" data={data} type='bun'/>
     <Ingredients title="Соусы" data={data} type='sauce'/>
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,15 +1,32 @@
 import React from 'react'
 import styles from './Ingredients.module.css'
-import { CurrencyIcon  } from '@ya.praktikum/react-developer-burger-ui-components'
+import PropTypes from 'prop-types';
+import { CurrencyIcon, Counter  } from '@ya.praktikum/react-developer-burger-ui-components'
 function Ingredients({title, data, type}) {
+
+  const dataPropTypes = PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    type: PropTypes.string.isRequired,
+  });
+  
+  Ingredients.propTypes = {
+    title: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    data: PropTypes.arrayOf(dataPropTypes).isRequired
+  }
+
   return (
     <div className={styles.main}>
           <h3 className={styles.title}>{title}</h3>
           <div className={styles.oneType}>
+
         {data && data.map(item=>{
           return(
             item.type === type && 
             <div key ={item._id} className={styles.oneItem}>
+          <Counter count={1} size="default" extraClass="m-1" />
               <img src={item.image} className={styles.img} alt={item.name}/>
     <div className={styles.priceSection}>
      <p className={styles.price}>{item.price}</p>
